8d459d3d0a403d4ba823a2bec6ef51e7bd2e14de
Wany <sung@wany.io>
2023-11-16 00:55:38 +0900

    update

--- start.sh
+++ start.sh
@@ -1,1 +1,10 @@
-java -Xms1G -Xmx2G -jar server.jar nogui
+JAR=server.jar
+OPTIONS=
+
+function start() { 
+  java -Xms1G -Xmx2G $OPTIONS -jar $JAR nogui
+  read -t 5 -p "Paused... (5sec)"
+  start
+}
+
+start
